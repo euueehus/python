3f460f5ee98d078752edc1e7f1528a745f98219d
euueehus <euueehusssss@gmail.com>
2023-10-15 11:02:28 +0800

    class3

---
+++ class 3/class 3.py
@@ -0,0 +1,33 @@
+# name = "python"
+# age = 31
+# new_str_1 = "我是%s,今年%d歲了" % (name, age)
+# print(new_str_1)
+"""while True:
+    try:
+        num = int(input("打數字"))
+        total = num + 1
+        print(total)
+    except:
+        print("你的電腦很爛(jokey○( ＾皿＾)っ Hehehe…)")
+    break"""
+
+# not 反轉
+# and 全部true =true
+# or 1個true =true
+# ()
+# **次方
+# +正 -負
+# * / % //
+# + -
+e = 0
+while True:
+    password = input("輸入密碼")
+    if password == "1234":
+        print("歡迎光臨")
+    else:
+        password != 1234
+    print("滾")
+
+    e += 1
+    if e == 3:
+        break
