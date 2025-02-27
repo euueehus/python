f91d159fa24bbbd59762161202752b47204a5045
euueehus <euueehusssss@gmail.com>
2025-02-27 19:06:31 +0800

    1

--- class13/hw13.py
+++ class13/hw13.py
@@ -48,8 +48,9 @@ while True:
     print("1. 新增支出記錄")
     print("2. 查詢特定日期的支出總和")
     print("3. 輸出所有記錄的總和")
-    print("4. 離開系統")
-    print(("5.存"))
+    print("4. 存錢")
+    print("5. 離開系統")
+    print(("6.存檔"))
     select = input("請輸入功能編號:")
     if select == "1":
         date = input("請輸入日期:")
@@ -60,11 +61,17 @@ while True:
         print(f"{date}的支出總和為{query_expenses(date)}")
     elif select == "3":
         print(f"所有記錄的總和為{total_expenses()}")
-    elif select == "5":
+    elif select == "4":
+        date = input("請輸入日期:")
+        amount = eval(input("請輸入金額:"))
+        add_expense(date, amount)
+        print("已處存")
+
+    elif select == "6":
         save()
         print("ok")
 
-    elif select == "4":
+    elif select == "5":
         break
     else:
         print("查無此功能請重新輸入")
