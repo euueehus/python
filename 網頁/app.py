627edddfd4c6f86df752ebb186ff9a3a2879df3c
euueehus <euueehusssss@gmail.com>
2025-09-08 22:02:36 +0800

    e

--- 網頁/app.py
+++ 網頁/app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("主頁.html")
+    return render_template("home.html")
 
 
-@app.route("/目錄")
+@app.route("/list")
 def liste():
-    return render_template("目錄.html")
+    return render_template("list.html")
 
 
 @app.route("/Kards")
@@ -18,24 +18,24 @@ def Kards_main():
     return render_template("Kards.html")
 
 
-@app.route("/Kards 美國")
+@app.route("/Kards_us")
 def Kards_us():
-    return render_template("Kards 美國.html")
+    return render_template("Kards_us.html")
 
 
-@app.route("/Kards 英國")
+@app.route("/Kards_uk")
 def Kards_uk():
-    return render_template("Kards 英國.html")
+    return render_template("Kards_uk.html")
 
 
-@app.route("/Kards 美國 很多解牌")
+@app.route("/Kards_us_many")
 def Kards_us_many():
-    return render_template("Kards 美國 很多解牌.html")
+    return render_template("Kards_us_many.html")
 
 
-@app.route("/Kards 美國 高油")
+@app.route("/Kards_us_high")
 def Kards_us_high():
-    return render_template("Kards 美國 高油.html")
+    return render_template("Kards_us_high.html")
 
 
 @app.route("/intro")
